6af9e50f7d322ae5c894d1ac857e34db40ef47e6
juanRCoder <guillecarck@gmail.com>
2025-03-04 22:31:03 -0500

    feat: added functions for sigIn with users

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  name: string
+  email: string
+  password: string
+}
+
+export class signInDto {
+  email: string
+  password: string
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private readonly users = [
+    {
+      id: 1,
+      name: 'john',
+      email: 'john@gmail.com',
+      password: 'john123',
+    },
+    {
+      id: 2,
+      name: 'juan',
+      email: 'juan@gmail.com',
+      password: 'juan123',
+    },
+  ];
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -12,8 +28,8 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(email: string): Promise<User | undefined> {
+    return this.users.find(user => user.email === email);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
