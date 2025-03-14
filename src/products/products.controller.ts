40f11f0137861ac4f185e134908989b64876727b
juanRCoder <guillecarck@gmail.com>
2025-03-13 22:10:50 -0500

    feat: added querys to paginate products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto, UpdateProductDto } from './dto/products.dto';
-
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  findAll(@Query('page') page?: string, @Query('limit') limit?: string) {
+    return this.productsService.findAll(Number(page), Number(limit));
   }
 
   @Get(':id')
@@ -23,10 +31,7 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateProductDto: UpdateProductDto
-  ) {
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(id, updateProductDto);
   }
 
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,36 +5,56 @@ import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class ProductsService {
-  constructor (private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-  async findAll() {
-    return this.prisma.product.findMany();
+  async findAll(page?: number, limit?: number) {
+    if (!page || !limit) {
+      const products = await this.prisma.product.findMany();
+      return products;
+    }
+    const offset = (page - 1) * limit;
+    const [products, total] = await Promise.all([
+      this.prisma.product.findMany({
+        skip: offset,
+        take: limit,
+      }),
+      this.prisma.product.count(),
+    ]);
+    return {
+      products,
+      pagination: {
+        products: products.length,
+        limit,
+        offset,
+        totalPages: Math.ceil(total / limit),
+      },
+    };
   }
 
   async findOne(id: string) {
     return await this.prisma.product.findUnique({
       where: { id },
-    })
+    });
   }
 
   async create(createProductDto: CreateProductDto) {
     return await this.prisma.product.create({
       data: {
         ...createProductDto,
-      }
-    })
+      },
+    });
   }
 
   async update(id: string, updateProductDto: UpdateUserDto) {
     return await this.prisma.product.update({
       where: { id },
-      data: updateProductDto
-    })
+      data: updateProductDto,
+    });
   }
 
   async remove(id: string) {
     return await this.prisma.product.delete({
-		  where: { id }
-	  })
+      where: { id },
+    });
   }
 }
