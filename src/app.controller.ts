c1b487ab04f7d640c4cda74f474a67bf6e9dd4c5
juanRCoder <guillecarck@gmail.com>
2025-04-17 16:46:55 -0500

    fix: change message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getRoot(): string {
-    return 'Servidor en funcionamiento';
+    return 'Server started on and connected';
   }
 }
